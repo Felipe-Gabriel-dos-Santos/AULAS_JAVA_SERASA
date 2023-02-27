f32fb5ebd80a893b171223da7ffe65f2d88c4d5f
Felipe-Gabriel-dos-Santos <vdoss2011@gmail.com>
2023-02-27 19:49:45 -0300

    first commit: variaveis java

---
+++ src/Variaveis/Variaveis.java
@@ -0,0 +1,22 @@
+package Variaveis;
+
+import java.util.jar.Attributes.Name;
+
+public class Variaveis {
+    
+    public static void Print() {
+        char sexo = 'M';
+        byte idade = 28;
+        short codigo = 204;
+        float troco = 2.45f;
+        int quantidade = 100;
+        int classes = 10;
+        long habitantes = 232325435;
+        double dolar = 2.50;
+        boolean alternativa = false;
+    
+        System.out.println("Sexo: " + sexo + " Idade: " + idade + " código: " + codigo);
+        System.out.println("Troco: " + troco + " Quantidade: " + quantidade + " Habitantes: " + habitantes);
+        System.out.println("Alternativa: " + alternativa + " Dolar: " + dolar + " Classes: " + classes);
+        }
+}
